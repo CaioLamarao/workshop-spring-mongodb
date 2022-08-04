2dc50750103ff4beb4eb10d6ec55dd328bb261b6
Caio Lamarão <caiolamarao@outlook.com>
2022-08-04 19:37:42 -0300

    GET Post by Id

---
+++ src/main/java/com/caiolamarao/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.caiolamarao.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.caiolamarao.workshopmongo.domain.Post;
+import com.caiolamarao.workshopmongo.repository.PostRepository;
+import com.caiolamarao.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("User not found"));
+	}
+}
