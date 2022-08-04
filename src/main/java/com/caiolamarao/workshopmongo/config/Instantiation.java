0674e68396113906fd9d28dd1e12fe83bb23b554
Caio Lamarão <caiolamarao@outlook.com>
2022-08-04 14:26:21 -0300

    DTO pattern to return Authors

--- src/main/java/com/caiolamarao/workshopmongo/config/Instantiation.java
+++ src/main/java/com/caiolamarao/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.caiolamarao.workshopmongo.domain.Post;
 import com.caiolamarao.workshopmongo.domain.User;
+import com.caiolamarao.workshopmongo.dto.AuthorDTO;
 import com.caiolamarao.workshopmongo.repository.PostRepository;
 import com.caiolamarao.workshopmongo.repository.UserRepository;
 
@@ -35,10 +36,12 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2022"), "Partiu Viagem", "Vou viajar para São Paulo, abraços", maria );
-		Post post2 = new Post(null, sdf.parse("23/03/2022") , "Bom dia", "Acordei Feliz hoje", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2022"), "Partiu Viagem", "Vou viajar para São Paulo, abraços", new AuthorDTO(maria) );
+		Post post2 = new Post(null, sdf.parse("23/03/2022") , "Bom dia", "Acordei Feliz hoje", new AuthorDTO(maria));
+		
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 
 	}
